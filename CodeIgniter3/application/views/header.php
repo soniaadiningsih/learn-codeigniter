e95043961be4a1ccd4e827cd25235432ef07f360
soniaadiningsih <sonia23an@gmail.com>
2020-04-14 18:22:42 +0700

    initial

---
+++ CodeIgniter3/application/views/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+
+    <title>Belajar</title>
+
+    <!-- bootstrap css -->
+    <link href="</php echo base_url('assets/bootstrap-4.4.1-dist/');?>css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- custom style -->
+    <link href="<?php echo base_url('assets/css/');?>style.css" rel="stylesheet">
+
+</head>
+<body>
+    
+
+
+
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+        </div>
+    <!-- /.container -->
+    </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?php echo base_url('assets/js/');?>jquery-3.4.1.min.js"></script>
+  <script src="<?php echo base_url('assets/bootstrap-4.4.1-dist/');?>js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
